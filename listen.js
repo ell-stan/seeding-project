08c0cbd3ae3428489b6b67fbd6f26eb176414097
ell-stan <202790043+ell-stan@users.noreply.github.com>
2025-04-28 11:29:58 +0100

    task 1 complete: added and set up app and listen files, set up test suite

---
+++ listen.js
@@ -0,0 +1,10 @@
+const app = require("./app.js");
+const port = 9090;
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server is listening on port ${port}...`);
+  }
+});
